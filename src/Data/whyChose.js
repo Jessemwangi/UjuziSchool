ecefcd3053ff3be12e470bf6f1d4b056ec5c3f25
JESSE <jessejzee@gmail.com>
2024-11-23 19:57:34 +0200

    return image to original

--- src/Data/whyChose.js
+++ src/Data/whyChose.js
@@ -1,7 +1,7 @@
 export const whyChose =[
     {
         color:'color-primary-style',
-        img:'https://res.cloudinary.com/dk4ruyonq/image/upload/c_thumb,w_200,g_face/v1731176177/Experiential_learning_of_STEM_fdfv8r.png',
+        img:'https://res.cloudinary.com/dk4ruyonq/image/upload/v1731176177/Experiential_learning_of_STEM_fdfv8r.png',
         icon:'45',
         title:'Experiential learning of STEM',
         text:'We enable experiential learning of STEM by combining theory, simulation, practical demonstration, project learning, and gamification.',
@@ -9,14 +9,14 @@ export const whyChose =[
     },
     {
         color:'color-secondary-style',
-        img:'https://res.cloudinary.com/dk4ruyonq/image/upload/c_thumb,w_200,g_face/v1731176188/Unlock_Applicability_dlyg6s.png',
+        img:'https://res.cloudinary.com/dk4ruyonq/image/upload/v1731176188/Unlock_Applicability_dlyg6s.png',
         icon:'46',
         title:'Unlock applicability of STEM',
         text:'We enable students to connect theoretical concepts learned in STEM subjects like physics with real-world applications.',
     },
     {
         color:'color-extra08-style',
-        img:'https://res.cloudinary.com/dk4ruyonq/image/upload/c_thumb,w_200,g_face/v1731176131/Hands_on_learning_bkf8x1.png',
+        img:'https://res.cloudinary.com/dk4ruyonq/image/upload/v1731176131/Hands_on_learning_bkf8x1.png',
         icon:'47',
         title:'Hands-on teaching methods',
         text:'Our teaching methods emphasize hands-on skills to prepare students for future STEM-related opportunities.',
